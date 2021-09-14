616b481794e103223d413dfb870822d34005da9d
lenboy11 <lennartherr@gmx.de>
2021-09-14 21:19:59 +0200

    Adding network representation

--- json2pdDF.py
+++ json2pdDF.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import numpy as np
 import json
+import networkx as nx
 
 ## parteien.json
 # WOMT_aParteien[index][0][0] = Partei Name
@@ -25,9 +26,37 @@ with open("json/parteienThesen.json") as f:
     partiesOpinion = json.load(f)
 
 # Due to the structure of partiesOpinion we consider the name of the parties as columns
-columns = []
+
+partyList = []
 for l in parties:
-    columns.append(l[0][1])
+    partyList.append(l[0][1].rstrip().lstrip())
 index = range(len(parties))
 
-df = pd.DataFrame( partiesOpinion, index = index, columns = columns )
+df = pd.DataFrame( partiesOpinion, index = index, columns = partyList )
+res = np.zeros((len(parties), len(parties)), dtype= np.int32)
+
+for c in range(len(partiesOpinion)):
+    for i in range(len(partiesOpinion[c])-1):
+        for j in range(i+1, len(partiesOpinion[c])):
+            res[i][j] += 1 - abs(partiesOpinion[c][i] - partiesOpinion[c][j])/2
+    
+dfRes = pd.DataFrame(res, partyList, partyList, dtype= np.int32)
+
+lineWidthList = []
+nodeSizeList = []
+G = nx.Graph()
+i = 0
+for party in partyList:
+    i += 1
+    G.add_node(party)
+    nodeSizeList.append((700*len(partyList))/(5*i+len(partyList)))
+for i in range(len(res)-1):
+    for j in range(i+1, len(res)):
+        G.add_edge( partyList[i], partyList[j], weight=3*res[i][j] )
+        lineWidthList.append(0.05 + 0.02*res[i][j])
+
+pos = nx.spring_layout(G) # positions for all nodes
+
+nx.draw_networkx_nodes(G, pos, node_size=nodeSizeList)
+nx.draw_networkx_labels(G, pos, font_size=20,font_family='sans-serif')
+nx.draw_networkx_edges(G, pos, width=lineWidthList)
